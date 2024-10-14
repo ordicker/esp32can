a8e3267f8e41c5d1921c961d46411525d6704024
Or <or.dicker@gmail.com>
2024-10-14 16:47:25 +0300

    ESP32-TWAI-CAN lib added

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,17 @@
+#include <Arduino.h>
+#include <ESP32-TWAI-CAN.hpp>
 
+// Default for ESP32
+#define CAN_TX 5
+#define CAN_RX 4
+
+void setup() {
+  Serial.begin(9600);
+  ESP32Can.setPins(CAN_TX, CAN_RX);
+}
+
+void loop() {
+  Serial.println("hello world!");
+  delay(1000);
+
+}
